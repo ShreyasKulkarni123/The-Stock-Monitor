00dfc5490d7a57691136dfa666091b421f621e83
shku7404@colorado.edu <root@SDM-014C-4-LTW.ucdenver.pvt>
2024-04-04 08:27:59 -0600

    Partial complete directory

---
+++ ProjectSourceCode/Test/server.spec.js
@@ -0,0 +1,32 @@
+// ********************** Initialize server **********************************
+
+const server = require('../index'); //TODO: Make sure the path to your index.js is correctly added
+
+// ********************** Import Libraries ***********************************
+
+const chai = require('chai'); // Chai HTTP provides an interface for live integration testing of the API's.
+const chaiHttp = require('chai-http');
+chai.should();
+chai.use(chaiHttp);
+const {assert, expect} = chai;
+
+// ********************** DEFAULT WELCOME TESTCASE ****************************
+
+describe('Server!', () => {
+  // Sample test case given to test / endpoint.
+  it('Returns the default welcome message', done => {
+    chai
+      .request(server)
+      .get('/welcome')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals('success');
+        assert.strictEqual(res.body.message, 'Welcome!');
+        done();
+      });
+  });
+});
+
+// *********************** TODO: WRITE 2 UNIT TESTCASES **************************
+
+// ********************************************************************************
