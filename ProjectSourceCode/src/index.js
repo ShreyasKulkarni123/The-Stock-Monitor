e3145078fc3515c6be9f557aa2104a5a02d7ccab
MyApaulogies <myapaulogies@tuta.io>
2024-04-07 23:12:30 -0600

    change bcrypt to bcryptjs

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
-const bcrypt = require('bcrypt'); //  To hash passwords
+const bcrypt = require('bcryptjs'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
 
 // *****************************************************
