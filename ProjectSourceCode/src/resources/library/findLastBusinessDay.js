fa61c5e69da14f635e32b927102e099f7160d514
anle7157 <anle7157@colorado.edu>
2024-04-18 08:54:33 -0600

    findLastBusinessDay finished

--- ProjectSourceCode/src/resources/library/findLastBusinessDay.js
+++ ProjectSourceCode/src/resources/library/findLastBusinessDay.js
@@ -1,17 +1,12 @@
 function isBusinessDay(date) {
-    // Check if the date is Saturday (6) or Sunday (0)
+    // if weekday then return 1
     return date.getDay() % 6 !== 0;
 }
 
 function findLastBusinessDay() {
     let currentDate = new Date();
-    
-    // Keep subtracting days until we find a business day
     while (!isBusinessDay(currentDate)) {
         currentDate.setDate(currentDate.getDate() - 1);
     }
-    
     return currentDate;
 }
-
-console.log(getMostRecentBusinessDay());
